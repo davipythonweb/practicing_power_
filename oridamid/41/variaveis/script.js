710a138ec03dcd4b36ebdba600083a11829d9ec1
Davi Nascimento <galaxys3tab32@gmail.com>
2024-04-29 09:36:12 +0000

    update class

--- oridamid/41/variaveis/script.js
+++ oridamid/41/variaveis/script.js
@@ -1,3 +1,65 @@
+
+// variaveis
+var pessoa = "Atonio"
+let idade = 29
+var possui_faculdade = true
+
+
 var nome = "Andre"
 
 console.info(nome)
+
+
+var preco = 25
+var totalComprado = 5
+var totalPreco = totalComprado * preco
+
+console.info(totalPreco)
+
+
+var sobrenome = "Rafael",
+    cidade = "Rio"
+
+console.info(sobrenome,cidade)
+
+
+var semDefinir
+
+console.info(semDefinir)
+
+// nao pode iniciar com numeros ou caracteres especiais(pode usar o $ ou _)
+// case sensitive => nome é diferente de Nome
+//  nao utilizar palavras reservadas
+// camel case => é comm nomearmos assim: abrirModal
+// nao usar acentos nem espacos
+
+/*
+  variaveis con const, nao eh possivel modificar(uma costante eh um valor unico)
+*/
+
+
+var time = 'vasco'
+var time = 'flamengo'
+
+console.log(time)
+
+/* exercio=>
+  Declarar uma variavel com o seu nome
+  declarar uma variavel com a sua idade
+  declarar uma varivael com a sua comida favorita e nao atribuir valor
+  atribuir valor a sua comida favorita
+  declarar 5 variaveis diferentes sem valores
+*/
+
+ let meuNome ="Eliote"
+ let minhaIdade = 39
+ let comidaFavorita
+ comidaFavorita = 'macarronada'
+
+ var musicaPreferia
+ var esportePreferido
+ var materiaPreferida
+ var corPreferida
+ var filmePreferido
+
+ console.log(meuNome,minhaIdade,comidaFavorita)
