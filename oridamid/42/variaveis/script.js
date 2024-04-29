14be2c9a8037e2e013cc6c25bf93fe553946d966
Davi Nascimento <galaxys3tab32@gmail.com>
2024-04-29 09:47:13 +0000

    update class

---
+++ oridamid/42/variaveis/script.js
@@ -0,0 +1,47 @@
+var sobrenome = "Rafael",
+    cidade = "Rio"
+
+console.info(sobrenome,cidade)
+
+
+var semDefinir
+
+console.info(semDefinir)
+
+// nao pode iniciar com numeros ou caracteres especiais(pode usar o $ ou _)
+// case sensitive => nome é diferente de Nome
+//  nao utilizar palavras reservadas
+// camel case => é comm nomearmos assim: abrirModal
+// nao usar acentos nem espacos
+
+/*
+  variaveis con const, nao eh possivel modificar(uma costante eh um valor unico)
+*/
+
+
+var time = 'vasco'
+var time = 'flamengo'
+
+console.log(time)
+
+
+// exercio=>
+// Declarar uma variavel com o seu nome
+let meuNome ="Eliote"
+// declarar uma variavel com a sua idade
+let minhaIdade = 39
+// declarar uma varivael com a sua comida favorita e nao atribuir valor
+let comidaFavorita
+// atribuir valor a sua comida favorita
+comidaFavorita = 'macarronada'
+// declarar 5 variaveis diferentes sem valores
+var professor, 
+    pais, 
+    endereco,
+    rua,
+    cep
+
+
+
+
+ console.log(meuNome,minhaIdade,comidaFavorita, professor)
