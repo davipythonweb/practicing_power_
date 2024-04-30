b1abcb32c9d5fe80a8f5b83cd3863fa8bdb7a860
Davi Nascimento <galaxys3tab32@gmail.com>
2024-04-30 04:10:16 +0000

    update class

---
+++ oridamid/43/variaveis/script.js
@@ -0,0 +1,17 @@
+var idade = 28
+var altura = 1.78
+
+// operadores aritmeticos
+var soma = 3 + 54 //soma
+var subtracao = 10 - 7 //subtracao
+var multiplicacao = 3 * 4 //multiplicacao
+var divisao = 40 - 20 //divisao
+var expoente = 10** 2 //exponenciacao
+var modulo = 14 % 5 //modulo
+
+// operadores aritmeticos(strings)
+
+var somaString = '100' + 50 // 10050
+var subtracaoString = '100' - 50 // 50
+var multiplicacaoString = '100' * '2' //200
+var divisaoString = 'Comprei 10' / 2 // NaN (not a number)
