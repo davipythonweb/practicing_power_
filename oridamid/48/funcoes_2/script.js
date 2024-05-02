2399d210909d8cd5ac670488e36a0a30f289f97e
Let's make <152368509+davipythonweb@users.noreply.github.com>
2024-05-02 01:31:15 +0000

    create arquivos aula-49

--- oridamid/48/funcoes_2/script.js
+++ oridamid/48/funcoes_2/script.js
@@ -1,2 +1,68 @@
 // Funçoes
 
+// Valores Retornados 
+// Uma Função pode retornar qualquer
+//  tipo de dados e até outras funçoes.
+
+function terceiraIdade(idade) {
+  if(typeof idade !== 'number') {
+    return 'Informe a sua idade!'
+  } else if(idade >= 60) {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(terceiraIdade(80))
+// OBS: cuidado, retornar diferentes tipos de dados 
+// na mesma função não eh uma boa ideia.
+
+// ESCOPO
+
+// Variaveis e funçoes definidas dentro de um bloco {} , 
+// não sao visiveis fora dele.
+
+function precisoVisitar(paisesVisitados) {
+  var totalPaises = 193
+  return `Ainda faltam ${totalPaises - paisesVisitados} paises para visitar! `
+}
+
+console.log(precisoVisitar(30))
+
+
+
+
+// ESCOPO Léxico
+// funções conseguem acessar variaveis 
+// que foram criadas no contexto pai.
+
+var profissao = 'Designer'
+
+function dados() {
+  var nome = 'André'
+  var idade = 28
+  function outrosDados() {
+    var endereco = 'Rio de Janeiro'
+    var idade = 29
+    return `${nome}. ${idade}, ${endereco}, ${profissao}`
+  }
+  return outrosDados()
+}
+
+console.info(dados()) // Retorna 'André, 29, Rio de Janeiro, Designer'
+// outrosDados() // retorna um erro
+
+
+
+
+// HOSTING
+// Antes de executar uma funçao, o Javascript 'move'
+// todas as funçoes declaradas para a memoria.
+
+imc(80, 1.80)
+
+function imc(peso, altura) {
+  const imc = peso / (altura ** 2)
+  console.info(imc)
+}
---
+++ oridamid/49/funcoes_3/script.js
@@ -0,0 +1,27 @@
+// Funções_3
+
+// EXERCÍCIO
+
+// Crie uma função para verificar se um valor eh Truthy
+
+// Crie uma funçao matematica que retorne o perimetro de um quadrado
+function calculaPerimetro(var1, var2, var3, var4) {
+  var perimetro = var1 + var2 + var3 + var4
+  return `O perimetro do seu quadrado eh ${perimetro} cm.`
+  
+}
+// lembrando: perimetro é a soma dos quatro lados do quadrado.
+
+
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parametro: nome e sobrenome
+
+// Crie uma função que verifica se um numero eh par.
+
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+
+// addEventListener eh uma funçao nativa do Javascript
+// o primeiro parâmetro eh o evento que ocorre e o segundo 
+// utilize essa funçao para mostrar no console o seu nome
+// quando o evento 'scroll' ocorrer.
