a75ac89fe0e68a725a0495074538038e726cfeb3
davipythonweb <galaxys3tab32@gmail.com>
2024-05-11 19:17:18 +0000

    query no database `index.html` `get.test.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -7,8 +7,16 @@ async function status(request, response) {
   const databaseVersionResult = await database.query("SHOW server_version;")
   const databaseVersionValue = databaseVersionResult.rows[0].server_version
 
-  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;")
-  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;"
+    );
+
+  const databaseMaxConnectionsValue =
+   databaseMaxConnectionsResult.rows[0].max_connections
+
+   const databaseOpenedConnectionsResult =  await database.query("SELECT * FROM pg_stat_activity;")
+   console.info(databaseOpenedConnectionsResult.rows)
+  
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
