cd52d9b5c461e760ac4f2b97058586ae2ff169f1
davipythonweb <galaxys3tab32@gmail.com>
2024-05-11 20:33:57 +0000

    protegendo de sql injection

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -13,7 +13,11 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.dependencies.database.opened_connections).toEqual(1)
 })
 
-test.only("Teste de SQL Injection", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --")
-})
+
+
+// test.only("Teste de SQL Injection", async () => {
+//   const response = await fetch("http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --")
+// })
+
+
 // Primeiro teste de integraçao
